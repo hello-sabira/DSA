b92dbee0490b49db8af7a60922c7fb1bfb673385
hello-sabira <sabira.khan248@gmail.com>
2022-01-23 19:18:12 +0300

    List Based Stack in Python

--- DSA-Python/List_Based_Stack.py
+++ DSA-Python/List_Based_Stack.py
@@ -1,3 +1,7 @@
+from _queue import Empty  # More efficient and better to use queue based stack
+
+
+# LIFO Stack implementation using a Python list
 class ArrayStack:
     def __init__(self):
         self.data = []
@@ -6,4 +10,19 @@ class ArrayStack:
         return len(self.data)
 
     def is_empty(self):
-        return len(self.data)==0
+        return len(self.data) == 0
+
+    def push(self, e):  # Add element to the top of stack
+        self.data.append(e)
+
+    def pop(self):      # Notice, no e here
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        else:
+            return self.pop()  # Remove last item from list
+
+    def peek(self):  # Sometimes also mentioned as top
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        else:
+            return self.data[-1]
