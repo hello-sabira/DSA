1ea3a6f339c6ab0e0fb0d8f56490e5cd36b61ef3
hello-sabira <sabira.khan248@gmail.com>
2022-01-23 18:37:17 +0300

    DSA Python

---
+++ DSA-Python/List_Based_Stack.py
@@ -0,0 +1,9 @@
+class ArrayStack:
+    def __init__(self):
+        self.data = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def is_empty(self):
+        return len(self.data)==0
